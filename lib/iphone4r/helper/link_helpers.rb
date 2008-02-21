a9a11e8243c8317d589b5d7d799b335663c4a600
schlueter <schlueter@e1c356e8-5827-0410-9592-da92de47e60c>
2008-02-21 03:02:07 +0000

    added external link helpers
    git-svn-id: http://svn.simpltry.com/plugins/iphone4r@28 e1c356e8-5827-0410-9592-da92de47e60c

--- lib/iphone4r/helper/link_helpers.rb
+++ lib/iphone4r/helper/link_helpers.rb
@@ -1,8 +1,24 @@
+#All of these link to helpers depend on a custom (included) version of iui being installed
+
 module Iphone4r
   module Helper
     module LinkHelpers
+      def _iphone4r_external_link_helper(text, url)
+        <<-END
+          <a href="#{url}" target="_external">#{text}</a>
+        END
+      end
       def link_to_map(text, q_value)
-        "<a href=\"http://maps.google.com/maps?q=#{q_value}\">#{text}</a>"
+        _iphone4r_external_link_helper(text, "http://maps.google.com/maps?q=#{q_value}")
+      end
+      
+      def link_to_you_tube(text, video_id)
+        _iphone4r_external_link_helper(text, "http://www.youtube.com/watch?v=#{video_id}")
+      end
+      
+      def link_to_phone(number, options = {})
+        text = options[:text] || number
+        _iphone4r_external_link_helper(text, "tel:#{number}")
       end
     end
   end
