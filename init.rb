f3c06c64f57d74ea2c04626b3da586c241040015
schlueter <schlueter@e1c356e8-5827-0410-9592-da92de47e60c>
2008-02-21 03:15:21 +0000

    added a version
    git-svn-id: http://svn.simpltry.com/plugins/iphone4r@29 e1c356e8-5827-0410-9592-da92de47e60c

--- init.rb
+++ init.rb
@@ -2,3 +2,7 @@ Mime::Type.register_alias "text/html", :iphone
 ActionView::Base.send :include, Iphone4r::Helper::Ibug
 ActionView::Base.send :include, Iphone4r::Helper::LinkHelpers
 ActionController::Base.send :include, Iphone4r::Controller::IphoneFormatFilter
+
+module Iphone4r
+  VERSION = "0.1"
+end
